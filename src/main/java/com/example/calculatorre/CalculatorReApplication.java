b8b3aef5125441747a14fae8053696b49c7993b3
daxxn <dahyeonhan4529@gmail.com>
2024-12-09 18:25:40 +0900

    feat : 초기 프로젝트 세팅

---
+++ src/main/java/com/example/calculatorre/CalculatorReApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculatorre/CalculatorReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatorre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
