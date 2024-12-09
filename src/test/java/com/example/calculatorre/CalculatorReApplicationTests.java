dad9acf5bac84ad9ac64a29e2accd3b9aa63b123
daxxn <dahyeonhan4529@gmail.com>
2024-12-09 20:55:43 +0900

    test(app): application 테스트 추가

--- src/test/java/com/example/calculatorre/CalculatorReApplicationTests.java
+++ src/test/java/com/example/calculatorre/CalculatorReApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.calculatorre;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CalculatorReApplicationTests {
-
+class CalculatorReApplicationTests extends NsTest {
     @Test
-    void contextLoads() {
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
     }
 
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        CalculatorReApplication.main(new String[]{});
+    }
 }
