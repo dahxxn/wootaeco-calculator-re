0f894b13f3ad05c6d295083941a3a59ae25e8806
daxxn <dahyeonhan4529@gmail.com>
2024-12-09 20:49:07 +0900

    feat(view): 출력 클래스 생성

---
+++ src/main/java/com/example/calculatorre/view/OutputView.java
@@ -0,0 +1,11 @@
+package com.example.calculatorre.view;
+
+public class OutputView {
+    public void printLineBreak() {
+        System.out.println();
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
