76c42861a09dcb98f09108bd9cfb83e847e289e3
daxxn <dahyeonhan4529@gmail.com>
2024-12-12 00:00:53 +0900

    test(app): 테스트 케이스 추가

--- src/test/java/com/example/calculatorre/CalculatorReApplicationTests.java
+++ src/test/java/com/example/calculatorre/CalculatorReApplicationTests.java
@@ -17,6 +17,14 @@ class CalculatorReApplicationTests extends NsTest {
     }
 
     @Test
+    void 커스텀_구분자_사용_2() {
+        assertSimpleTest(() -> {
+            run("//;\\n10,200:30;4");
+            assertThat(output()).contains("결과 : 244");
+        });
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("-1,2,3"))
