1009b368ff556c806e057ad9d1d5b3a1c22f432c
daxxn <dahyeonhan4529@gmail.com>
2024-12-11 23:51:16 +0900

    fix(controller): 메서드 오류 수정

--- src/test/java/com/example/calculatorre/CalculatorControllerTest.java
+++ src/test/java/com/example/calculatorre/CalculatorControllerTest.java
@@ -10,42 +10,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/*
-@Test
-public void 일반_확인_테스트() {
-    int expected = 1000;
-    int actual = 1000;
-    assertEquals(expected, actual);
-
-    //혹은 값 반환을 하지 않는 메서드를 테스트하는 경우
-    assertDoesNotThrow(() -> {
-    });
-}
-
-@Test
-public void 예외발생_확인_테스트() {
-    assertThatThrownBy(() -> {
-    })
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("ExceptionMessage.ERROR_MESSAGE_INPUT_PURCHASE_INFO_NOT_EXIST_PRODUCT.toString()");
-}
-
-@Test
-public void 참_반환_테스트() {
-    assertTrue(true);
-}
-
-@Test
-public void 거짓_반환_테스트() {
-    assertFalse(false);
-}
-
-@ParameterizedTest
-@ValueSource(strings = {"hello", "  hi  "})
-public void 여러_값_테스트(String testInput) {
-
-}
-*/
 
 public class CalculatorControllerTest {
     CalculatorController controller = new CalculatorController();
@@ -78,6 +42,6 @@ public class CalculatorControllerTest {
     @Test
     public void 구분자로_문자열_조각_테스트() {
         String rawInput = "1,2:3";
-        assertEquals(new ArrayList<>(List.of(new String[]{"1", "2", "3"})), controller.getInputToken(rawInput));
+        assertEquals(new ArrayList<>(List.of(new String[]{"1", "2", "3"})), controller.splitInputByDelimiter(rawInput));
     }
 }
